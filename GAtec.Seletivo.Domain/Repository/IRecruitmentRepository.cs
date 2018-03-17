2b096a2ec5a264564fdfdbeae01725b326d99a4d
Cleiton Osti <osti.rdp@gmail.com>
2018-03-17 13:28:34 -0300

    Add repository

---
+++ GAtec.Seletivo.Domain/Repository/IRecruitmentRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GAtec.Seletivo.Domain.Model;
+
+namespace GAtec.Seletivo.Domain.Repository
+{
+    interface IRecruitmentRepository: IBaseRepository<Recruitment>
+    {
+    }
+}
