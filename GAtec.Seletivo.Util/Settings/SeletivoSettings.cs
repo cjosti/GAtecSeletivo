449f2b41977e792e08f200bbf6bcfd8ac0b55b46
Cleiton Osti <osti.rdp@gmail.com>
2018-03-17 14:19:09 -0300

    add connection string

---
+++ GAtec.Seletivo.Util/Settings/SeletivoSettings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace GAtec.Seletivo.Util.Settings
+{
+    public static class SeletivoSettings
+    {
+        public static string applicationName
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["ApplicationName"];
+            }
+        }
+
+        public static string connectionString
+        {
+            get
+            {
+                return ConfigurationManager.ConnectionStrings["Seletivo"].ConnectionString;
+            }
+        }
+
+    }
+}
